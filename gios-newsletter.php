685a0b3f5969e601a3082e7b9bfef361ff2eb7fe
Walter McConnell <Walter.McConnell@asu.edu>
2017-10-10 16:30:46 -0700

    Cleanup Base Plugin File
    Modified the gios-newsletter.php file to add GitHub URLs for the project, and myself (for now), as well as update the plugin's description - which shows up on the WordPress plugin list.

--- gios-newsletter.php
+++ gios-newsletter.php
@@ -14,11 +14,11 @@
  *
  * @wordpress-plugin
  * Plugin Name:       GIOS Newsletter
- * Plugin URI:        www.example.com
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Plugin URI:        https://github.com/gios-asu/
+ * Description:       A plugin to allow for WordPress to create our news digest and related newsletter(s)
  * Version:           1.0.0
  * Author:            Walter McConnell
- * Author URI:        www.example.com/walt
+ * Author URI:		  https://github.com/wmcconnell
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       gios-newsletter
